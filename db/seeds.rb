8ce0ec3f338c52bd9ab509deb5b7ca9aef610ed6
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-02-16 15:06:06 +0100

    Pushing solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database"
+Restaurant.destroy_all
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Creating 5 restaurants..."
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: CATEGORIES.sample
+  )
+end
+
+puts "Done!"
